23549ca7e82cd6d2b97f8a46cf10de9b2bb122a2
AndreaPupo28 <andreapupo2002@gmail.com>
2025-02-12 18:37:19 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -48,7 +48,8 @@ if __name__ == "__main__":
     evaluate_model(model, test_loader, criterion, device)
 
     model.eval()
-    current_log = random.choice(dataset.data["activity"].tolist())
+    random_sequence, _ = random.choice(dataset.data)  # Prende solo X
+    current_log = " ".join(random_sequence)  # Converti la sequenza in stringa
     predicted_next, probabilities = predict_next_log(model, tokenizer, current_log, dataset.label_map, device)
 
     print(f"\nLog attuale: {current_log}")
