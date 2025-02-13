7635b4a7f45c5fb83d4bb6501c6be912ef92a4f7
AndreaPupo28 <andreapupo2002@gmail.com>
2025-02-13 15:00:32 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -52,5 +52,5 @@ if __name__ == "__main__":
     current_log = " ".join(random_sequence[-5:]) # Converti la sequenza in stringa
     predicted_next, probabilities = predict_next_log(model, tokenizer, current_log, dataset.label_map, device)
 
-    print(f"\nLog attuale: {current_log}")
+    print(f"\nSequenza di input per il modello: {current_log}")
     print(f"Log successivo predetto: {predicted_next}")
