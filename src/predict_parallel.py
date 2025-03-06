1e1bf5b81723ce5f2ff5a0631375f488d2930f27
AndreaPupo28 <andreapupo2002@gmail.com>
2025-03-06 16:33:42 +0100

    Update predict_parallel.py

--- src/predict_parallel.py
+++ src/predict_parallel.py
@@ -4,14 +4,15 @@ from constraints_checker import check_constraints
 from constraints import constraints
 from activity import ActivityPrediction
 
-def predict_parallel_sequences(model, tokenizer, initial_log, label_map, device, k=5):
+def predict_parallel_sequences(model, tokenizer, initial_log, label_map, device, k=5): # consideriamo i 5 log più probabili
     model.eval()
     sequences = [[initial_log]]
     final_sequences = []
 
     while sequences:
         new_sequences = []
-        for seq in sequences[:10]:
+        print(f"\nAttuali sequenze in elaborazione: {[' → '.join([act.name if isinstance(act, ActivityPrediction) else act for act in seq]) for seq in sequences]}")
+        for seq in sequences:
             current_log = " → ".join([act.name if isinstance(act, ActivityPrediction) else act for act in seq])
             with torch.no_grad():
                 inputs = tokenizer(
@@ -24,7 +25,7 @@ def predict_parallel_sequences(model, tokenizer, initial_log, label_map, device,
                 logits = model(inputs["input_ids"], inputs["attention_mask"])
                 probs = torch.nn.functional.softmax(logits, dim=1).cpu().numpy().flatten()
 
-            sorted_indices = np.argsort(probs)[::-1]
+            sorted_indices = np.argsort(probs)[::-1] # ordina gli indici delle probabilità in modo decrescente
             valid_candidates = []
 
             #print(f"\nPredizioni per la sequenza '{current_log}':")
@@ -37,9 +38,9 @@ def predict_parallel_sequences(model, tokenizer, initial_log, label_map, device,
                 if check_constraints(new_sequence, constraints, detailed=False, completed=True):
                     valid_candidates.append(ActivityPrediction(candidate_log, candidate_prob))
 
-            if valid_candidates:
+            if valid_candidates: # controlla se ci sono candidati che rispettano i vincoli
                 for candidate in valid_candidates:
-                    new_sequences.append(seq + [ActivityPrediction(candidate.name, candidate.probability)])
+                    new_sequences.append(seq + [ActivityPrediction(candidate.name, candidate.probability)]) # concatena la sequenza corrente con la nuova attività
                     #print(f"  → Aggiunta nuova sequenza: {' → '.join([act.name if isinstance(act, ActivityPrediction) else act for act in seq + [candidate]])}")
             else:
                 final_sequences.append(seq)
@@ -47,8 +48,9 @@ def predict_parallel_sequences(model, tokenizer, initial_log, label_map, device,
 
         sequences = new_sequences
 
-    #print("\nSequenze finali generate:")
-    #for seq in final_sequences:
-        #print(f"  {' → '.join(seq)}")
 
-    return [[(activity.name, activity.probability) for activity in seq] for seq in final_sequences]
+    print("\nSequenze finali generate:")
+    for seq in final_sequences:
+        print(f"  {' → '.join(seq)}")
+
+    return [[(activity.name, activity.probability) for activity in seq] for seq in final_sequences] #lista di tuple
