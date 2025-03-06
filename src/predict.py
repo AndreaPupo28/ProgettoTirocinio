36f4da6da488c4b0dc983038ce487c8bd53786ae
AndreaPupo28 <andreapupo2002@gmail.com>
2025-03-06 16:35:20 +0100

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,4 +1,4 @@
 from predict_parallel import predict_parallel_sequences
 
-def predict_next_log_with_constraints(model, tokenizer, current_log, label_map, device, num_candidates=2):
+def predict_next_log_with_constraints(model, tokenizer, current_log, label_map, device, num_candidates):
     return predict_parallel_sequences(model, tokenizer, current_log, label_map, device, num_candidates)
