bf05106e03e1dc51cc0f12afaaf7dd2763ddc088
AndreaPupo28 <andreapupo2002@gmail.com>
2025-03-06 16:35:55 +0100

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -7,7 +7,6 @@ from activity import ActivityPrediction
 class LogDataset(Dataset):
     def __init__(self, file_path, tokenizer, max_length=128): # Costruttore
         self.data = pd.read_csv(file_path, low_memory=False)
-        self.data = self.data.sample(frac=0.1, random_state=42)
         print(f"Dataset caricato: {len(self.data)} righe trovate.")
 
         self.tokenizer = tokenizer
@@ -20,12 +19,13 @@ class LogDataset(Dataset):
         sequences = []
         for case in grouped:
             for i in range(1, len(case)):
-                sequences.append((case[:i], ActivityPrediction(case[i], 1.0)))  # Probabilità 1.0 perché è dato dal dataset  # X = [A1, A2, ..., Ai], Y = Ai+1
+                sequences.append((case[:i], ActivityPrediction(case[i], 1.0)))  # Probabilità 1.0 perché è dato dal dataset. X = [A1, A2, ..., Ai], Y = Ai+1
 
         self.data = sequences
 
+        # estrae tutte le attività uniche dal dataset, includendo sia le sequenze di input che le attività predette, rimuove i duplicati e le ordina alfabeticamente.
         unique_activities = sorted(set(a.name if isinstance(a, ActivityPrediction) else a for seq in self.data for a in seq[0] + [seq[1]]))
-        self.label_map = {label: idx for idx, label in enumerate(unique_activities)} # Crea un dizionario
+        self.label_map = {label: idx for idx, label in enumerate(unique_activities)} # Crea un dizionario (nomeAttività-indice)
         self.num_classes = len(self.label_map)
 
         #print(f"Classi trovate: {self.label_map}")
