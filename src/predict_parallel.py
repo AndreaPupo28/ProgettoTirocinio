c4977d91b2f437c7781e68c77d98f8182470534c
AndreaPupo28 <andreapupo2002@gmail.com>
2025-03-07 14:29:19 +0100

    Update predict_parallel.py

--- src/predict_parallel.py
+++ src/predict_parallel.py
@@ -11,7 +11,7 @@ def predict_parallel_sequences(model, tokenizer, initial_log, label_map, device,
 
     while sequences:
         new_sequences = []
-        #print(f"\nAttuali sequenze in elaborazione: {[' → '.join([act.name if isinstance(act, ActivityPrediction) else act for act in seq]) for seq in sequences]}")
+        print(f"\nAttuali sequenze in elaborazione: {[' → '.join([act.name if isinstance(act, ActivityPrediction) else act for act in seq]) for seq in sequences]}")
         for seq in sequences:
             current_log = " → ".join([act.name if isinstance(act, ActivityPrediction) else act for act in seq])
             with torch.no_grad():
