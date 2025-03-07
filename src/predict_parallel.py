e04cf4d32a56af4a03d43a8aa653a52b85489055
AndreaPupo28 <andreapupo2002@gmail.com>
2025-03-07 16:02:39 +0100

    Update predict_parallel.py

--- src/predict_parallel.py
+++ src/predict_parallel.py
@@ -4,12 +4,14 @@ from constraints_checker import check_constraints
 from constraints import constraints
 from activity import ActivityPrediction
 
-def predict_parallel_sequences(model, tokenizer, initial_log, label_map, device, k=5):
+def predict_parallel_sequences(model, tokenizer, initial_log, label_map, device, k=2, max_iterations=3):
     model.eval()
     sequences = [[initial_log]]
     final_sequences = []
+    iteration = 0
 
-    while sequences:
+    while sequences and iteration < max_iterations:
+        iteration += 1
         new_sequences = []
         
         # --- Stampa solo le prime 3 sequenze ---
@@ -22,7 +24,7 @@ def predict_parallel_sequences(model, tokenizer, initial_log, label_map, device,
             print(f"  {i+1}) {seq_str}")
         if len(sequences) > 3:
             print(f"... e altre {len(sequences) - 3} sequenze non visualizzate.")
-        # --- Fine modifica ---
+        # --- Fine stampa ---
         
         for seq in sequences:
             current_log = " → ".join(
@@ -41,18 +43,14 @@ def predict_parallel_sequences(model, tokenizer, initial_log, label_map, device,
 
             sorted_indices = np.argsort(probs)[::-1]  # ordina gli indici delle probabilità in modo decrescente
             valid_candidates = []
-
-            #print(f"\nPredizioni per la sequenza '{current_log}':")
             for idx in sorted_indices[:k]:
                 candidate_log = list(label_map.keys())[idx]
                 candidate_prob = probs[idx]
                 new_sequence = current_log + " " + candidate_log
-                #print(f"  Candidato: {candidate_log}, Probabilità: {candidate_prob:.4f}")
-
                 if check_constraints(new_sequence, constraints, detailed=False, completed=True):
                     valid_candidates.append(ActivityPrediction(candidate_log, candidate_prob))
 
-            if valid_candidates:  # controlla se ci sono candidati che rispettano i vincoli
+            if valid_candidates:
                 for candidate in valid_candidates:
                     new_sequences.append(seq + [ActivityPrediction(candidate.name, candidate.probability)])
             else:
@@ -66,4 +64,5 @@ def predict_parallel_sequences(model, tokenizer, initial_log, label_map, device,
         print(f"  {' → '.join(seq)}")
 
     return [[(activity.name, activity.probability) for activity in seq] for seq in final_sequences]
+
 
