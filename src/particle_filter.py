05615f57847fc0e7faf7c1eec5edd0c885694fc1
AndreaPupo28 <andreapupo2002@gmail.com>
2025-03-10 18:24:33 +0100

    Update particle_filter.py

--- src/particle_filter.py
+++ src/particle_filter.py
@@ -6,6 +6,9 @@ from activity import ActivityPrediction
 from interactive_constraint_manager import InteractiveConstraintManager
 import random
 
+# Variabile globale per gestire la terminazione del processo
+process_terminated = False
+
 class ParticleFilter:
     def __init__(self, model, tokenizer, label_map, device, num_particles=50):
         self.model = model
@@ -16,24 +19,14 @@ class ParticleFilter:
         self.particles = []
         self.constraint_manager = InteractiveConstraintManager()
 
-    def initialize_particles(self, initial_activities):
-        self.particles = [[ActivityPrediction(activity, 1.0)] for activity in initial_activities]
-        print("\n--------------------------------------")
-        print("Inizio della generazione delle particelle iniziali")
-        print("--------------------------------------\n")
-        for particle in self.particles:
-            print(f"Particella iniziale: {[act.name for act in particle]}")
-
-    def sense_environment(self, particles):
-        return constraints + self.constraint_manager.get_constraints()
-
     def step(self):
+        global process_terminated
         new_particles = []
         current_length = len(self.particles[0]) if self.particles else 0
 
         self.constraint_manager.request_constraints(current_length)
 
-        MAX_PARTICLES = 10000  # Imposta il limite massimo di particelle
+        MAX_PARTICLES = 10000
 
         for particle in self.particles:
             if len(new_particles) >= MAX_PARTICLES:
@@ -44,6 +37,7 @@ class ParticleFilter:
             predicted_sequences = predict_next_log_with_constraints(
                 self.model, self.tokenizer, input_text, self.label_map, self.device, num_candidates=2
             )
+
             if not predicted_sequences or not predicted_sequences[0]:
                 print(f"Fine della traccia per la particella: {[act.name for act in particle]} - nessuna nuova attività da predire.")
                 continue
@@ -56,6 +50,10 @@ class ParticleFilter:
                 if check_constraints(" ".join([act.name for act in new_particle]), current_constraints, detailed=False, completed=True):
                     new_particles.append(new_particle)
                     print(f"Prossima attività predetta: {predicted_name} con probabilità {predicted_prob:.4f}\n")
+
+        if not new_particles:
+            print("Nessuna nuova particella generata. Terminazione del processo.")
+            process_terminated = True
 
         self.particles = new_particles
 
