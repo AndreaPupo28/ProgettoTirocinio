bec071a672049e764c758aac929bea82de09d967
AndreaPupo28 <andreapupo2002@gmail.com>
2025-03-12 19:21:24 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ from ipywidgets import widgets
 from IPython.display import display, clear_output
 import json
 import time
+import numpy as np
 
 if __name__ == "__main__":
     model_name = "prajjwal1/bert-medium"
